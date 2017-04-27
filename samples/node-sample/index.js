cb7c483c7f3f29c85aba9c777ce0fbd436f11f34
Brendan Hagan <bhaganksu@gmail.com>
2017-04-26 20:11:26 -0500

    Fix webpack distributable
    * Switch back from ws.on('open') to ws.onopen()
    * Remove worthless Status.wrap method
    * Fix incoming message parsing

--- samples/node-sample/index.js
+++ samples/node-sample/index.js
@@ -1,14 +1,13 @@
 const OBSWebSocket = require('../../index.js');
 const ws = new OBSWebSocket();
 
-ws.logger.setLevel('info');
-
 // Declare some events to listen for.
 ws.onConnectionOpened(() => {
   console.log('Connection Opened');
 
   // Send some requests.
   ws.getSceneList(null, (err, data) => {
+    throw "this";
     console.log(err, data);
   });
 
@@ -17,8 +16,4 @@ ws.onConnectionOpened(() => {
   });
 });
 
-// Open the connection and Authenticate if needed. URL defaults to localhost:4444
-// ws = new OBSWebSocket('address', 'password');
-//
-// ws.connect(); // ws.connect('localhost');
-// ws.authenticate('password');
+ws.connect();
