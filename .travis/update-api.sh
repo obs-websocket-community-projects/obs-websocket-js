904910c915e1f66d02b9128625c1d00b35fd4cfb
Brendan Hagan <C_Hagan.Brendan@Cerner.com>
2017-08-24 09:52:49 -0500

    ci: Generate/update api at end of travis builds

---
+++ .travis/update-api.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset
+TARGET_BRANCH="master"
+
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]] || [[ "$TRAVIS_BRANCH" != "$TARGET_BRANCH" ]]
+then
+  echo "Skipping $TARGET_BRANCH api update."
+  exit 0
+fi
+
+npm run update-api
+
+# Apply all changes on top of the latest master commit.
+git remote add upstream "https://$GH_TOKEN@$GH_REF"
+git fetch upstream
+git reset upstream/$TARGET_BRANCH
+git add -A
+git commit -m "api(ci): Update available obs-websocket methods"
+git push -q upstream HEAD:$TARGET_BRANCH
