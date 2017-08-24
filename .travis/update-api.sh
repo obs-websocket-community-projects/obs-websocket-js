4cf3a8c6635382180a7cdd2f3a6b6440cc01900d
Brendan Hagan <C_Hagan.Brendan@Cerner.com>
2017-08-24 10:01:51 -0500

    ci: duplicate git upstream assignment

--- .travis/update-api.sh
+++ .travis/update-api.sh
@@ -11,9 +11,9 @@ fi
 npm run update-api
 
 # Apply all changes on top of the latest master commit.
-git remote add upstream "https://$GH_TOKEN@$GH_REF"
+# git remote add upstream "https://$GH_TOKEN@$GH_REF"
 git fetch upstream
 git reset upstream/$TARGET_BRANCH
-git add -A
+git add lib/API.js
 git commit -m "api(ci): Update available obs-websocket methods"
 git push -q upstream HEAD:$TARGET_BRANCH
