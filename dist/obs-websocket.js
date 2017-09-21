f9b2ec3d3f54b9a67b9de7457ac51bb365808940
travis-ci <travis@travis-ci.org>
2017-09-21 15:39:23 +0000

    gh-pages: (dev) 6cf99e2801a610e9282da22ab2c3bacb016ceffc

--- dist/obs-websocket.js
+++ dist/obs-websocket.js
@@ -3,7 +3,7 @@
  * Author: Brendan Hagan (haganbmj)
  * License: MIT
  * Repository: https://github.com/haganbmj/obs-websocket-js
- * Build Timestamp: 2017-09-14 15:38:02+00:00
+ * Build Timestamp: 2017-09-21 15:38:46+00:00
  * Built from Commit: https://github.com/haganbmj/obs-websocket-js/commit/6cf99e2801a610e9282da22ab2c3bacb016ceffc
  */
 var OBSWebSocket =
