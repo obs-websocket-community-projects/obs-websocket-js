7a9a3b3fa67e961af77107c006fef4aedb2260d7
Brendan Hagan <bhaganksu@gmail.com>
2018-11-20 22:26:16 -0500

    wip: attempt to add more clearly typed definitions for obs messages

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,9 +3,21 @@
 import { EventEmitter } from 'events';
 export = obs_websocket_js;
 
+interface ObsMessage {
+  messageId: string;
+  status: "ok";
+  [propName: string]: any;
+}
+
+interface ObsError {
+  messageId: string;
+  status: "error";
+  error: string;
+}
+
 declare class obs_websocket_js extends EventEmitter {
   constructor();
-  connect(options?: {address?: string; password?: string}): Promise<void>;
+  connect(options?: {address?: string; password?: string}, callback?: (error?: Error) => void): Promise<void>;
   disconnect(): void;
-  send(requestType: string, args?: object, callback?: (error: Error, ...args: any[]) => void): Promise<any>;
+  send(requestType: string, args?: object, callback?: (error?: ObsError, data?: ObsMessage) => void): Promise<ObsMessage>;
 }
