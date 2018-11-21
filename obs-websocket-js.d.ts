3ca226234139728b0bee4e1bc7c5bbd768c5098b
Alex Van Camp <vancamp.alex@gmail.com>
2018-11-20 19:38:15 -0600

    wip: add type tests/linting

--- obs-websocket-js.d.ts
+++
@@ -1,9 +0,0 @@
-import {EventEmitter} from 'events';
-export = obs_websocket_js;
-
-declare class obs_websocket_js extends EventEmitter {
-  constructor();
-  connect(options?: {address?: string; password?: string}): Promise<void>;
-  disconnect(): void;
-  send(requestType: string, args: object, callback?: Function): Promise<any>;
-}
