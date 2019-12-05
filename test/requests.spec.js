c017e2e4471b941f2d2557a0399b768f96e0aa53
Brendan Hagan <bhaganksu@gmail.com>
2019-12-04 21:59:29 -0500

    chore: upgrade major dependency versions, fix tests, tslint, etc.

--- test/requests.spec.js
+++ test/requests.spec.js
@@ -7,7 +7,7 @@ const obs = new OBSWebSocket();
 
 test.before(async t => {
   unauthServer = await env.makeServer(4445);
-  await t.notThrows(obs.connect({
+  await t.notThrowsAsync(obs.connect({
     address: 'localhost:4445'
   }));
 });
@@ -17,23 +17,27 @@ test.after.always('cleanup', () => {
 });
 
 test('resolves when a valid request is sent', async t => {
-  await t.notThrows(obs.send('ValidMethodName'));
+  await t.notThrowsAsync(obs.send('ValidMethodName'));
 });
 
 test('rejects when an invalid request is sent', async t => {
-  const resp = await t.throws(obs.send('InvalidMethodName'));
-  t.deepEqual(resp.status, 'error');
-  t.deepEqual(resp.error, 'invalid request type');
+  try {
+    await obs.send('InvalidMethodName');
+    t.fail('expected promise rejection');
+  } catch (e) {
+    t.is(e.status, 'error');
+    t.is(e.error, 'invalid request type');
+  }
 });
 
 test('permits null args', async t => {
-  await t.notThrows(obs.send('ValidMethodName', null));
+  await t.notThrowsAsync(obs.send('ValidMethodName', null));
 });
 
 test.cb('sendCallback -- success case', t => {
   obs.sendCallback('ValidMethodName', {}, (err, data) => {
     t.falsy(err);
-    t.deepEqual(data.status, 'ok');
+    t.is(data.status, 'ok');
     t.end();
   });
 });
@@ -41,7 +45,7 @@ test.cb('sendCallback -- success case', t => {
 test.cb('sendCallback -- omitted args', t => {
   obs.sendCallback('ValidMethodName', (err, data) => {
     t.falsy(err);
-    t.deepEqual(data.status, 'ok');
+    t.is(data.status, 'ok');
     t.end();
   });
 });
@@ -49,22 +53,28 @@ test.cb('sendCallback -- omitted args', t => {
 test.cb('sendCallback -- error case', t => {
   obs.sendCallback('InvalidMethodName', {}, (err, data) => {
     t.falsy(data);
-    t.deepEqual(err.status, 'error');
+    t.is(err.status, 'error');
     t.end();
   });
 });
 
 test('rejects when no open connection exists', async t => {
   const obs2 = new OBSWebSocket();
-  const resp = await t.throws(obs2.send('ValidMethodName'));
-
-  t.deepEqual(resp.status, 'error');
-  t.deepEqual(resp.code, 'NOT_CONNECTED');
+  try {
+    await obs2.send('ValidMethodName');
+    t.fail('expected promise rejection');
+  } catch (e) {
+    t.is(e.status, 'error');
+    t.is(e.code, 'NOT_CONNECTED');
+  }
 });
 
 test('rejects when no request type is specified', async t => {
-  const resp = await t.throws(obs.send());
-
-  t.deepEqual(resp.status, 'error');
-  t.deepEqual(resp.code, 'REQUEST_TYPE_NOT_SPECIFIED');
+  try {
+    await obs.send();
+    t.fail('expected promise rejection');
+  } catch (e) {
+    t.is(e.status, 'error');
+    t.is(e.code, 'REQUEST_TYPE_NOT_SPECIFIED');
+  }
 });
