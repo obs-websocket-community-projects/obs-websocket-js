bb950a63824e42f64d895ce912001f85867b1a70
t2t2 <taavot@gmail.com>
2022-11-13 18:19:55 +0200

    Update node example

--- samples/node-sample/index.js
+++ samples/node-sample/index.js
@@ -1,17 +1,17 @@
-const { OBSWebSocket } = require('obs-websocket-js');
+const { default: OBSWebSocket } = require('obs-websocket-js');
 const obs = new OBSWebSocket();
 
 // Declare some events to listen for.
 obs.on('ConnectionOpened', () => {
   console.log('Connection Opened');
+});
+
+obs.on('Identified', () => {
+	console.log('Identified, good to go!')
 
   // Send some requests.
-  obs.sendCallback('GetSceneList', {}, (err, data) => {
-    console.log('Using callbacks:', err, data);
-  });
-
-  obs.send('GetSceneList').then(data => {
-    console.log('Using promises:', data);
+  obs.call('GetSceneList').then((data) => {
+    console.log('Scenes:', data);
   });
 });
 
@@ -19,4 +19,8 @@ obs.on('SwitchScenes', data => {
   console.log('SwitchScenes', data);
 });
 
-obs.connect();
+obs.connect('ws://localhost:4455', 'password').then((info) => {
+	console.log('Connected and identified', info)
+}, () => {
+	console.error('Error Connecting')
+});
